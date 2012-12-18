775c87ae487cbbc9f99cf6d14a20c260f107eb41
John Cleveland <John.Cleveland.CA@Gmail.com>
2012-12-17 16:29:12 -0800

    Added an extra parenthesis to avert crashes. Still getting compile error at L388 C13.

--- data/scripts/world/corvus/RestockSpawnPoint.java
+++ data/scripts/world/corvus/RestockSpawnPoint.java
@@ -301,166 +301,165 @@ public class RestockSpawnPoint extends BaseSpawnPoint {
 	    CargoAPI aCargo = station.getCargo();//get cargo
 	    if (aCargo.isFreeTransfer() == false){// only if the station is not the player station
 		if (station.getName().toString().equals("Serenity Station" == false)){// Also not the Neutral Serenity Station
-			FleetDataAPI aMothFleet = aCargo.getMothballedShips();//get fleet data
-                        
-			// All stations are restocked with crew and ships from aShips, aWings
-			
-			restockWeapons(aCargo);//restock weapons (all)
-			restockCrew(aCargo);//restock crew
-			
-			restockShips(aMothFleet, aShips);
-			restockFighters(aMothFleet, aWings);
-                        
-			//Global.getSector().addMessage(station.getName().toString());
-                        
-			// Individual stations are restocked from their own ship/wing lists                               
-			
-			if (station.getName().toString().equals("Orbital Station")){
-			    restockShips(aMothFleet,hShips);
-			    restockFighters(aMothFleet,hWings);
-			    Global.getSector().addMessage("Checking Orbital Station");
-			}
-			
-			if (station.getName().toString().equals("Hidden Base")){
-			    restockShips(aMothFleet,pShips);
-			    restockFighters(aMothFleet,pWings);
-			    Global.getSector().addMessage("Checking Hidden Base");
-			}
-                        
-			if (station.getName().toString().equals("Corporate HQ")){
-			    restockShips(aMothFleet,tShips);
-			    restockFighters(aMothFleet,tWings);
-			    Global.getSector().addMessage("Checking Corporate HQ");
-			}
-			
-			// Add individual stations here! Use the above as templates.
-                        
-			//Interstellar Federation
-                        if (station.getName().toString().equals("Navy Outpost")){
-			    restockShips(aMothFleet,fShips);
-			    restockFighters(aMothFleet,fWings);
-			    Global.getSector().addMessage("Checking Navy Outpost");				
-			}
-			
-			//Junk Pirates station
-			if (station.getName().toString().equals("Junk Yard")){
-			    restockShips(aMothFleet,jShips);
-			    restockFighters(aMothFleet,jWings);
-			    Global.getSector().addMessage("Checking Junk Yard");
-			};
-
-			//Lotus Pirates Station
-			if (station.getName().toString().equals("Anchorage")){
-			    restockShips(aMothFleet,lShips);
-			    restockFighters(aMothFleet,lWings);
-			    Global.getSector().addMessage("Checking Lotus Pirates Anchorage");
-			};
-
-			//Nuetrino Corp Station
-			if (station.getName().toString().equals("Solar Powerplant")){
-			    restockShips(aMothFleet,nShips);
-			    restockFighters(aMothFleet,nWings);
-			    Global.getSector().addMessage("Checking Solar Powerplant");
-			};
-
-			//Shadowyard Industries Station
-			if (station.getName().toString().equals("Outlet Plant")){
-			    restockShips(aMothFleet,sShips);
-			    restockFighters(aMothFleet,sWings);
-			    Global.getSector().addMessage("Checking Outlet plant");
-			};
-			//Independant and Orphan Station
-			if (station.getname().toString().equals("Trade Station")){
-			    restockShips(aMothFleet,dShips,mShips,aspShips);
-			    restockFighters(aMothFleet,dWings,mWings,aspWings);
-			    Global.getSector().addMessage("Checking Trade Station");
-			};
-                        
-			
+		    FleetDataAPI aMothFleet = aCargo.getMothballedShips();//get fleet data
+                    
+		    // All stations are restocked with crew and ships from aShips, aWings
+		    
+		    restockWeapons(aCargo);//restock weapons (all)
+		    restockCrew(aCargo);//restock crew
+		    
+		    restockShips(aMothFleet, aShips);
+		    restockFighters(aMothFleet, aWings);
+                    
+		    //Global.getSector().addMessage(station.getName().toString());
+                    
+		    // Individual stations are restocked from their own ship/wing lists                               
+		    
+		    if (station.getName().toString().equals("Orbital Station")){
+			restockShips(aMothFleet,hShips);
+			restockFighters(aMothFleet,hWings);
+			Global.getSector().addMessage("Checking Orbital Station");
 		    }
+		    
+		    if (station.getName().toString().equals("Hidden Base")){
+			restockShips(aMothFleet,pShips);
+			restockFighters(aMothFleet,pWings);
+			Global.getSector().addMessage("Checking Hidden Base");
 		    }
-		
-		return null;
-	    }
-	    
-	    
-    
-	    private void restockShips(FleetDataAPI mothFleet, String[] ships){
-		Set shipSet = new HashSet(Arrays.asList(ships));
-		
-		List mothShips = mothFleet.getMembersListCopy();
-		Set msSet = new HashSet();
-		for (int i = 0; i < mothShips.size(); i++) {
-		    String specId = ((FleetMemberAPI) mothShips.get(i)).getSpecId();
-		    msSet.add(specId);
-		}
-		
-		shipSet.removeAll(msSet);
-		
-		//Global.getSector().addMessage("Moth Size = " + msSet.size());
-		//Global.getSector().addMessage("Set Size = " + shipSet.size());
-		
-		Iterator iter = shipSet.iterator();
-		
-		while(iter.hasNext()) {
-		    mothFleet.addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP,(String) iter.next()));
-		}
-	    }
-	    
-	    private void restockFighters(FleetDataAPI mothFleet, String[] wings){
-		Set wingSet = new HashSet(Arrays.asList(wings));
-		
-		List mothShips = mothFleet.getMembersListCopy();
-		Set msSet = new HashSet();
-		for (int i = 0; i < mothShips.size(); i++) {
-		    String specId = ((FleetMemberAPI) mothShips.get(i)).getSpecId();
-		    msSet.add(specId);
-		}
-		
-		
-		wingSet.removeAll(msSet);
-	   		//Global.getSector().addMessage("Moth Size = " + msSet.size());
-		//Global.getSector().addMessage("Set Size = " + wingSet.size());
-		
-		Iterator iter = wingSet.iterator();
-		
-		while(iter.hasNext()) {
-		    mothFleet.addFleetMember(Global.getFactory().createFleetMember(
-										   FleetMemberType.FIGHTER_WING,(String) iter.next()));
-		}
-	    }
-	    
-	    private void restockWeapons(CargoAPI sCargo){
-		List allWeapons = Global.getSector().getAllWeaponIds();
-		
-		for (int i=0; i<allWeapons.size(); i++){
-		    if (sCargo.getNumWeapons((String) allWeapons.get(i)) == 0){
-			sCargo.addWeapons((String) allWeapons.get(i), 4);
+                    
+		    if (station.getName().toString().equals("Corporate HQ")){
+			restockShips(aMothFleet,tShips);
+			restockFighters(aMothFleet,tWings);
+			Global.getSector().addMessage("Checking Corporate HQ");
 		    }
+		    
+		    // Add individual stations here! Use the above as templates.
+                    
+		    //Interstellar Federation
+		    if (station.getName().toString().equals("Navy Outpost")){
+			restockShips(aMothFleet,fShips);
+			restockFighters(aMothFleet,fWings);
+			Global.getSector().addMessage("Checking Navy Outpost");				
+		    }
+		    
+		    //Junk Pirates station
+		    if (station.getName().toString().equals("Junk Yard")){
+			restockShips(aMothFleet,jShips);
+			restockFighters(aMothFleet,jWings);
+			Global.getSector().addMessage("Checking Junk Yard");
+		    };
+		    
+		    //Lotus Pirates Station
+		    if (station.getName().toString().equals("Anchorage")){
+			restockShips(aMothFleet,lShips);
+			restockFighters(aMothFleet,lWings);
+			Global.getSector().addMessage("Checking Lotus Pirates Anchorage");
+		    };
+		    
+		    //Nuetrino Corp Station
+		    if (station.getName().toString().equals("Solar Powerplant")){
+			restockShips(aMothFleet,nShips);
+			restockFighters(aMothFleet,nWings);
+			Global.getSector().addMessage("Checking Solar Powerplant");
+		    };
+		    
+		    //Shadowyard Industries Station
+		    if (station.getName().toString().equals("Outlet Plant")){
+			restockShips(aMothFleet,sShips);
+			restockFighters(aMothFleet,sWings);
+			Global.getSector().addMessage("Checking Outlet plant");
+		    };
+		    //Independant and Orphan Station
+		    if (station.getname().toString().equals("Trade Station")){
+			restockShips(aMothFleet,dShips,mShips,aspShips);
+			restockFighters(aMothFleet,dWings,mWings,aspWings);
+			Global.getSector().addMessage("Checking Trade Station");
+		    };
+                    
+		    
 		}
-		
 	    }
 	    
-	    private void restockCrew(CargoAPI sCargo){
-		if (sCargo.getCrew(CargoAPI.CrewXPLevel.GREEN) == 0){
-		    sCargo.addCrew(CargoAPI.CrewXPLevel.GREEN, 200);
-		}
-		if (sCargo.getCrew(CargoAPI.CrewXPLevel.REGULAR) == 0){
-		    sCargo.addCrew(CargoAPI.CrewXPLevel.REGULAR, 100);
-		}
-		if (sCargo.getCrew(CargoAPI.CrewXPLevel.VETERAN) == 0){
-		    sCargo.addCrew(CargoAPI.CrewXPLevel.VETERAN, 25);
-		}
-		if (sCargo.getCrew(CargoAPI.CrewXPLevel.ELITE) == 0){
-		    sCargo.addCrew(CargoAPI.CrewXPLevel.ELITE, 5);
-		}
-		if (sCargo.getSupplies() < 100){
-		    sCargo.addSupplies(250);
-		}
-		
+	    return null;
+	}
+	
+	
+	
+	private void restockShips(FleetDataAPI mothFleet, String[] ships){
+	    Set shipSet = new HashSet(Arrays.asList(ships));
+	    
+	    List mothShips = mothFleet.getMembersListCopy();
+	    Set msSet = new HashSet();
+	    for (int i = 0; i < mothShips.size(); i++) {
+		String specId = ((FleetMemberAPI) mothShips.get(i)).getSpecId();
+		msSet.add(specId);
 	    }
 	    
+	    shipSet.removeAll(msSet);
+	    
+	    //Global.getSector().addMessage("Moth Size = " + msSet.size());
+	    //Global.getSector().addMessage("Set Size = " + shipSet.size());
+	    
+	    Iterator iter = shipSet.iterator();
+	    
+	    while(iter.hasNext()) {
+		mothFleet.addFleetMember(Global.getFactory().createFleetMember(FleetMemberType.SHIP,(String) iter.next()));
+	    }
+	}
+	
+	private void restockFighters(FleetDataAPI mothFleet, String[] wings){
+	    Set wingSet = new HashSet(Arrays.asList(wings));
+	    
+	    List mothShips = mothFleet.getMembersListCopy();
+	    Set msSet = new HashSet();
+	    for (int i = 0; i < mothShips.size(); i++) {
+		String specId = ((FleetMemberAPI) mothShips.get(i)).getSpecId();
+		msSet.add(specId);
+	    }
+	    
+	    
+	    wingSet.removeAll(msSet);
+	    //Global.getSector().addMessage("Moth Size = " + msSet.size());
+	    //Global.getSector().addMessage("Set Size = " + wingSet.size());
+	    
+	    Iterator iter = wingSet.iterator();
+	    
+	    while(iter.hasNext()) {
+		mothFleet.addFleetMember(Global.getFactory().createFleetMember(
+									       FleetMemberType.FIGHTER_WING,(String) iter.next()));
+	    }
+	}
+	
+	private void restockWeapons(CargoAPI sCargo){
+	    List allWeapons = Global.getSector().getAllWeaponIds();
+	    
+	    for (int i=0; i<allWeapons.size(); i++){
+		if (sCargo.getNumWeapons((String) allWeapons.get(i)) == 0){
+		    sCargo.addWeapons((String) allWeapons.get(i), 4);
+		}
+	    }
 	    
 	}
+	
+	private void restockCrew(CargoAPI sCargo){
+	    if (sCargo.getCrew(CargoAPI.CrewXPLevel.GREEN) == 0){
+		sCargo.addCrew(CargoAPI.CrewXPLevel.GREEN, 200);
+	    }
+	    if (sCargo.getCrew(CargoAPI.CrewXPLevel.REGULAR) == 0){
+		sCargo.addCrew(CargoAPI.CrewXPLevel.REGULAR, 100);
+	    }
+	    if (sCargo.getCrew(CargoAPI.CrewXPLevel.VETERAN) == 0){
+		sCargo.addCrew(CargoAPI.CrewXPLevel.VETERAN, 25);
+	    }
+	    if (sCargo.getCrew(CargoAPI.CrewXPLevel.ELITE) == 0){
+		sCargo.addCrew(CargoAPI.CrewXPLevel.ELITE, 5);
+	    }
+	    if (sCargo.getSupplies() < 100){
+		sCargo.addSupplies(250);
+	    }
+	    
+	}
+	
+	
     }
 }
